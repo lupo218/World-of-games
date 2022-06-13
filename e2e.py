b1c4ef84749a37b3a633b6a7451a75cff0ae1a8d
lupo218 <63341151+lupo218@users.noreply.github.com>
2022-06-13 11:49:55 +0300

    Update e2e.py

--- e2e.py
+++ e2e.py
@@ -1,32 +1,37 @@
-"""
-# Filename: run_selenium.py
-"""
-
-## Run selenium and chrome driver to scrape data from cloudbytes.dev
 import time
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 
-## Setup chrome options
-chrome_options = Options()
-chrome_options.add_argument("--headless") # Ensure GUI is off
-chrome_options.add_argument("--no-sandbox")
+def Check_URL(url,xpat):
+    ## Setup chrome options
+    chrome_options = Options()
+    chrome_options.add_argument("--headless") # Ensure GUI is off
+    chrome_options.add_argument("--no-sandbox")
 
-# Set path to chromedriver as per your configuration
-webdriver_service = Service("/usr/bin/chromedriver")
+    # Set path to chromedriver as per your configuration
+    webdriver_service = Service("/usr/bin/chromedriver")
 
-# Choose Chrome Browser
-browser = webdriver.Chrome(service=webdriver_service, options=chrome_options)
+    # Choose Chrome Browser
+    browser = webdriver.Chrome(service=webdriver_service, options=chrome_options)
 
-# Get page
-browser.get("http://192.168.0.5/")
+    # Get page
+    browser.get(url)
 
-# Extract description from page and print
-description = browser.find_element(By.XPATH, '/html/body/h1/div')
-print(f"{description.text}")
+    # Extract description from page and print
+    description = browser.find_element(By.XPATH, xpat)
+    #Wait for 10 seconds
+    time.sleep(2)
+    browser.quit()
+    return description.text
 
-#Wait for 10 seconds
-time.sleep(10)
-browser.quit()
+print(Check_URL('http://192.168.0.5/','/html/body/h1/div'))
+
+
+
+
+
+
+#"http://192.168.0.5/"
+#'/html/body/h1/div'
