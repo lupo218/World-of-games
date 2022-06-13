6615877ada7d5e73fa9715b6c2af195bb7cd5c87
lupo218 <63341151+lupo218@users.noreply.github.com>
2022-06-13 11:32:37 +0300

    Update e2e.py

--- e2e.py
+++ e2e.py
@@ -3,8 +3,9 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 chrome_options = Options()
-chrome_options.add_argument("--headless") # Ensure GUI is off
+# chrome_options.add_argument("--headless") # Ensure GUI is off
 chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument('--headless')
 
 def Check_URL(url):
     my_driver = webdriver.Chrome(executable_path='/usr/bin/chromedriver')
