dd0ad84f2c16f92b0be33df36db3336b13c56496
lupo218 <63341151+lupo218@users.noreply.github.com>
2022-06-13 11:43:06 +0300

    Update e2e.py

--- e2e.py
+++ e2e.py
@@ -1,13 +1,48 @@
+"""
+# Filename: run_selenium.py
+"""
+
+## Run selenium and chrome driver to scrape data from cloudbytes.dev
+import time
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
-chrome_options = Options()
-# chrome_options.add_argument("--headless") # Ensure GUI is off
-chrome_options.add_argument("--no-sandbox")
-chrome_options.add_argument('--headless')
 
-def Check_URL(url):
+## Setup chrome options
+chrome_options = Options()
+chrome_options.add_argument("--headless") # Ensure GUI is off
+chrome_options.add_argument("--no-sandbox")
+
+# Set path to chromedriver as per your configuration
+webdriver_service = Service("/usr/bin/chromedriver")
+
+# Choose Chrome Browser
+browser = webdriver.Chrome(service=webdriver_service, options=chrome_options)
+
+# Get page
+browser.get("http://192.168.0.5/")
+
+# Extract description from page and print
+description = browser.find_element(By.XPATH, '/html/body/h1/div')
+print(f"{description.text}")
+
+#Wait for 10 seconds
+time.sleep(10)
+browser.quit()
+
+
+
+
+
+
+
+
+
+
+
+
+
     my_driver = webdriver.Chrome(executable_path='/usr/bin/chromedriver')
     browser = my_driver(service=webdriver_service, options=chrome_options)
     browser.get(url)
