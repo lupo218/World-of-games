3cec82172d000ae7a6006ff86ae5f298b540e5ff
lupo218 <63341151+lupo218@users.noreply.github.com>
2022-06-13 13:30:54 +0300

    Update e2e.py

--- e2e.py
+++ e2e.py
@@ -4,34 +4,24 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 
-def Check_URL(url,xpat):
-    ## Setup chrome options
-    chrome_options = Options()
-    chrome_options.add_argument("--headless") # Ensure GUI is off
-    chrome_options.add_argument("--no-sandbox")
+## Setup chrome options
+chrome_options = Options()
+chrome_options.add_argument("--headless") # Ensure GUI is off
+chrome_options.add_argument("--no-sandbox")
 
-    # Set path to chromedriver as per your configuration
-    webdriver_service = Service("/usr/bin/chromedriver")
+# Set path to chromedriver as per your configuration
+webdriver_service = Service("/usr/bin/chromedriver")
 
-    # Choose Chrome Browser
-    browser = webdriver.Chrome(service=webdriver_service, options=chrome_options)
+# Choose Chrome Browser
+browser = webdriver.Chrome(service=webdriver_service, options=chrome_options)
 
-    # Get page
-    browser.get(url)
+# Get page
+browser.get("http://192.168.0.5/")
 
-    # Extract description from page and print
-    description = browser.find_element(By.XPATH, xpat)
-    #Wait for 10 seconds
-    time.sleep(2)
-    browser.quit()
-    return description.text
+# Extract description from page and print
+description = browser.find_element(By.XPATH, '/html/body/h1/div')
+print(f"{description.text}")
 
-print(Check_URL('http://192.168.0.5/','/html/body/h1/div'))
-
-
-
-
-
-
-#"http://192.168.0.5/"
-#'/html/body/h1/div'
+#Wait for 10 seconds
+time.sleep(2)
+browser.quit()
