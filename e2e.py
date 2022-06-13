3ce2b804c7810605fba723c1945b981a4ecfc6a3
lupo218 <63341151+lupo218@users.noreply.github.com>
2022-06-13 13:46:45 +0300

    n

--- e2e.py
+++ e2e.py
@@ -4,24 +4,31 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 
-## Setup chrome options
-chrome_options = Options()
-chrome_options.add_argument("--headless") # Ensure GUI is off
-chrome_options.add_argument("--no-sandbox")
+def Check_url(url,xpt):
+    ## Setup chrome options
+    chrome_options = Options()
+    chrome_options.add_argument("--headless") # Ensure GUI is off
+    chrome_options.add_argument("--no-sandbox")
 
-# Set path to chromedriver as per your configuration
-webdriver_service = Service("/usr/bin/chromedriver")
+    # Set path to chromedriver as per your configuration
+    webdriver_service = Service("/usr/bin/chromedriver")
 
-# Choose Chrome Browser
-browser = webdriver.Chrome(service=webdriver_service, options=chrome_options)
+    # Choose Chrome Browser
+    browser = webdriver.Chrome(service=webdriver_service, options=chrome_options)
 
-# Get page
-browser.get("http://192.168.0.5/")
+    # Get page
+    browser.get(url)
 
-# Extract description from page and print
-description = browser.find_element(By.XPATH, '/html/body/h1/div')
-print(f"{description.text}")
+    # Extract description from page and print
+    description = browser.find_element(By.XPATH, xpt)
+    result =None
+    result = description.text
 
-#Wait for 10 seconds
-time.sleep(2)
-browser.quit()
+    #Wait for 10 seconds
+    time.sleep(2)
+    browser.quit()
+    return result
+
+
+result = Check_url("http://192.168.0.5/",'/html/body/h1/div')
+print(result)
