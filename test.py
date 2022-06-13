230466f1c5a7417894f4ad168de4b4127d3cd72c
lupo218 <63341151+lupo218@users.noreply.github.com>
2022-06-13 13:37:49 +0300

    Create test.py

---
+++ test.py
@@ -0,0 +1,30 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+
+def Check_url():
+    ## Setup chrome options
+    chrome_options = Options()
+    chrome_options.add_argument("--headless") # Ensure GUI is off
+    chrome_options.add_argument("--no-sandbox")
+
+    # Set path to chromedriver as per your configuration
+    webdriver_service = Service("/usr/bin/chromedriver")
+
+    # Choose Chrome Browser
+    browser = webdriver.Chrome(service=webdriver_service, options=chrome_options)
+
+    # Get page
+    browser.get("http://192.168.0.5/")
+
+    # Extract description from page and print
+    description = browser.find_element(By.XPATH, '/html/body/h1/div')
+    print(f"{description.text}")
+
+    #Wait for 10 seconds
+    time.sleep(2)
+    browser.quit()
+
+Check_url()
